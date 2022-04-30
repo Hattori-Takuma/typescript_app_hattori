11603a7ca3719ab464d900110b64bc59eb5c7039
Hattori-Takuma <hattori.ta@icloud.com>
2022-04-30 14:14:50 +0900

    typescript

---
+++ src/App.tsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const isFlag1: boolean = true;
+  const isFlag2: boolean = false;
+  // const isFlag3: boolean = 'aaa';
+
+  const num1: number = 10;
+  const num2: number = -10;
+  // const num3: number = 'aaa';
+
+  const str1: string = 'aaa';
+  // const str2: string = -10;
+  // const str3: string = false;
+
+  let temp: string[] = ['adf', 'kdk', 'iku'];
+  let temp2: Array<string> = ['adf', 'kdk', 'iku'];
+
+  let taple: [string, number] = ['hattori', 9999];
+
+  let profile1: any = 123456;
+  let profile2: any = 'test';
+  let profile3: any = false;
+
+
+  // const test = (): void => {
+  //   return 'hello'
+  // };
+
+  const error = (number: string): never => {
+    throw new Error();
+  };
+
+  console.log(error('hello'));
+
+  let object2: { id: number, name: string } = { id: 1, name: 'hattori' };
+
+  type TestType = {
+    id: number,
+    name: string
+  }
+
+  let object1: TestType = { id: 1, name: 'hattori' };
+
+  interface ObjectInterface {
+    id: number;
+    name: string;
+  }
+  let object3: ObjectInterface = { id: 1, name: 'hattori' };
+
+  const kansu = (): number => 43;
+
+  let numberAny: any = kansu();
+  let numberUnkouwn: unknown = kansu();
+
+  let sum1 = 10 + numberAny;
+  // let sum2 = 10 + numberUnknown;
+
+  type Pitcher1 = {
+    throwingSpeed: number;
+  };
+
+  type Batter1 = {
+    battingAverage: number;
+  };
+
+  type TwoWayPlayer = Pitcher1 & Batter1;
+
+  let ootani: TwoWayPlayer = {
+    throwingSpeed: 154,
+    battingAverage: 3.65,
+  }
+
+  interface mondai{
+    id: number;
+    name: string;
+    url: string;
+    isStart: false;
+  }
+
+  const mondai = {
+    id: 1,
+    name: 'mondai',
+    url: 'https://mondai.com',
+    isStart: false,
+  }
+
+  
+  
+  
+
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
